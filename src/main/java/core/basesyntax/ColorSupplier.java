08d8c98f03b43c9bdc1ad4e44dda50208f62b28b
Adam Juroszek <adam.juroszek1@gmail.com>
2026-08-20 10:08:54 +0200

    ColorSUpplier improvements

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,9 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    public Color getRandomColor() {
+    private static final int index = new Random().nextInt(Color.values().length);
 
-        int index = new Random().nextInt(Color.values().length);
+    public Color getRandomColor() {
         Color color = Color.values()[index];
         return color;
     }
