728d0a9af8770c52f632461f63b17161852dd44f
Adam Juroszek <adam.juroszek1@gmail.com>
2026-08-20 12:23:49 +0200

    fix ColorSupplier logic

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
-    private final int index = new Random().nextInt(Color.values().length);
-    private final Color color = Color.values()[index];
+    private final Random random = new Random();
 
     public Color getRandomColor() {
-        return color;
+        int index = random.nextInt(Color.values().length);
+        return Color.values()[index];
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,14 +3,14 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private final Color randomColor = new ColorSupplier().getRandomColor();
-    private final int MAX_BALL_NUMBER = 100;
+    private final ColorSupplier colorSupplier = new ColorSupplier();
+    private final int maxBallNumber = 100;
     private final Random random = new Random();
 
     public Ball getRandomBall() {
         Ball randomBall = new Ball();
-        randomBall.setColor(randomColor);
-        randomBall.setNumber(random.nextInt(MAX_BALL_NUMBER));
+        randomBall.setColor(colorSupplier.getRandomColor());
+        randomBall.setNumber(random.nextInt(maxBallNumber));
         return randomBall;
     }
 }
