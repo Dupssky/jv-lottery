b562e2c9f12aa9345dac5b1baf9cacb929927108
Adam Juroszek <adam.juroszek1@gmail.com>
2026-08-20 11:22:45 +0200

    changed MAX_BALL_NUMBER to 100

--- src/main/java/core/basesyntax/NumberSupplier.java
+++ src/main/java/core/basesyntax/NumberSupplier.java
@@ -3,10 +3,10 @@ package core.basesyntax;
 import java.util.Random;
 
 public class NumberSupplier {
-    private final int index = 101;
+    private final int MAX_BALL_NUMBER = 100;
     private final Random random = new Random();
 
     public int getRandomNumber() {
-        return random.nextInt(index);
+        return random.nextInt(MAX_BALL_NUMBER);
     }
 }
