b563239209fba8a6b94bd37593d24f65a5a73ea7
Adam Juroszek <adam.juroszek1@gmail.com>
2026-08-20 10:37:06 +0200

    added setters & getters for Ball class

--- src/main/java/core/basesyntax/Ball.java
+++ src/main/java/core/basesyntax/Ball.java
@@ -5,6 +5,22 @@ public class Ball {
     private Color color;
     private int number;
 
+    public Color getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
     @Override
     public String toString() {
         return "The " + this.color + " ball " + "has number: " + this.number;
